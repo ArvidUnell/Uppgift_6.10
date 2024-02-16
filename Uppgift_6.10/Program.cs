98398b33d1f9404945be1815a365d6194293bf04
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-16 10:43:09 +0100

    Add project files.

---
+++ Uppgift_6.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift_6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en sträng");
+            Console.WriteLine($"Den strängen innehöll {AntalTalIText(Console.ReadLine())} tal.");
+        }
+        static int AntalTalIText(string meddelande)
+        {
+            string[] meddelandeArr = meddelande.Split(' ');
+
+            int summa = 0;
+            double x;
+
+            foreach (string text in meddelandeArr)
+            {
+                if (double.TryParse(text,out x))
+                {
+                    summa++;
+                }
+            }
+
+            return summa;
+        }
+    }
+}
